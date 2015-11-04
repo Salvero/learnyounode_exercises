ddf7cd85f306536d50ced8a07496ade08d536030
Salman Akram <salmanit2012@gmail.com>
2015-11-04 00:30:23 -0500

    add First Async I/O

---
+++ first_async_io.js
@@ -0,0 +1,12 @@
+// #4 - First Async I/O
+
+var fs = require('fs')
+var file = process.argv[2]
+
+fs.readFile(file, function (err, contents) {
+
+    // fs.readFile(file, 'UT8', callback) can also be used
+    var lines = contents.toString().split('\n').length - 1
+
+    console.log(lines)
+})
