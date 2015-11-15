e93b3d39fb3991c3033705ddf6c6fc3c1c60206a
Salman Akram <salmanit2012@gmail.com>
2015-11-14 19:17:09 -0500

    add web apps with Express.js

---
+++ jade_3.js
@@ -0,0 +1,19 @@
+# Exercise 3 - JADE
+
+// Create an Express.js app with a home page rendered by Jade template engine.
+//
+// The homepage should respond to /home.
+//
+// The view should show the current date using toDateString.
+
+var express = require('express')
+var app = express()
+
+app.set('view engine', 'jade')
+app.set('views', process.argv[3])
+
+app.get('/home', function(req, res) {
+  res.render('index', {date: new Date().toDateString()})
+})
+
+app.listen(process.argv[2])
